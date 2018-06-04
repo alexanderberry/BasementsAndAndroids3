01f656764d01e192d3ae5ccb65b3d24ed0b709a0
TheCompSciNoob <kyroschow@gmail.com>
2018-06-03 22:39:17 -0700

    PlayerActivity Created

---
+++ app/src/main/java/com/example/testaccount/basementsandandroids3/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testaccount.basementsandandroids3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
