b34689ffc6fc947b59f07fd902d68796bfa9198c
TheCompSciNoob <kyroschow@gmail.com>
2018-06-03 22:57:28 -0700

    PlayerActivity Created

--- app/src/main/java/com/example/testaccount/basementsandandroids3/MainActivity.kt
+++ app/src/main/java/com/example/testaccount/basementsandandroids3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testaccount.basementsandandroids3
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testaccount.basementsandandroids3.R.id.button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
