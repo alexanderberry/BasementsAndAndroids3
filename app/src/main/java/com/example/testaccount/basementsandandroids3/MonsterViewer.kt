5a535feaecbe944a757a7b51aeb1323401e26e6b
Ryan Liu <ryanliu8888@yahoo.com>
2018-06-03 22:43:39 -0700

    new activity

---
+++ app/src/main/java/com/example/testaccount/basementsandandroids3/MonsterViewer.kt
@@ -0,0 +1,13 @@
+package com.example.testaccount.basementsandandroids3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MonsterViewer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monster_viewer)
+
+    }
+}
