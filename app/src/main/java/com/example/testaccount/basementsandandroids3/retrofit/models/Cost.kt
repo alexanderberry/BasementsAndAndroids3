fcd923003dce0cbf91bd3f5ad5406c8d9502d976
JKemanian <zombieoftheweek@gmail.com>
2018-06-03 22:09:34 -0700

    imported repo

--- app/src/main/java/com/example/testaccount/basementsandandroids3/retrofit/models/Cost.kt
+++
@@ -1,9 +0,0 @@
-package com.example.testaccount.basementsandandroids3.retrofit.models
-
-/**
- * Created by chichow25 on 3/21/18.
- */
-data class Cost(
-        val quantity: Int,
-        val unit: String
-)
