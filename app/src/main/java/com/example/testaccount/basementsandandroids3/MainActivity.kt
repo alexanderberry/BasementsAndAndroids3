54b731b4968ae5c2909793c64c6cb132a216da6b
Alexander Berry <berryalex123@gmail.com>
2018-06-03 23:10:18 -0700

    Images now fit to buttons

--- app/src/main/java/com/example/testaccount/basementsandandroids3/MainActivity.kt
+++ app/src/main/java/com/example/testaccount/basementsandandroids3/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.testaccount.basementsandandroids3
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.testaccount.basementsandandroids3.R.id.button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {
+        dmButton.setOnClickListener {
             val intent = Intent(this, DMActivity::class.java)
             startActivity(intent)
         }
